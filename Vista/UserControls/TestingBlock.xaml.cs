7c3afe8191a5662f150c34b3a947b20bf105144d
Mosnterinhouse <lucas.cruz2003@hotmail.com>
2026-05-05 18:15:55 -0300

    Uploaded first batch of work

---
+++ Vista/UserControls/TestingBlock.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Controls;
+
+namespace WPF_Test.Vista.UserControls
+{
+    /// <summary>
+    /// Lógica de interacción para TestingBlock.xaml
+    /// </summary>
+    public partial class TestingBlock : UserControl
+    {
+        public TestingBlock()
+        {
+            InitializeComponent();
+        }
+
+        private string placeholderText;
+        public string PlaceholderText
+        {
+            get { return placeholderText; }
+            set 
+            {
+                placeholderText = value;
+                btn.Content = placeholderText;
+            }
+        }
+
+    }
+}
